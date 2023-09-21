c798746d791cb147de52ee3207292458ea9e4937
jamesome <jamesome1@gmail.com>
2023-09-21 21:40:25 +0900

    컴포넌트, 공통모듈 분리

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<a href="/product">Product</a><br>
-<a href="/product">Product2</a><br>
+<a href="/items">Items</a><br>
---
+++ src/scripts/redirectToUrl.ts
@@ -0,0 +1,3 @@
+export function redirectToUrl(url: string) {
+    window.location.href = url;
+}
